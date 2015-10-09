ed850adf72334dfa7dcd449fd20fe0e1670f1727
xinyu.jiang <xinyu.jiang@qunar.com>
2015-10-09 20:12:27 +0800

    页面

--- src/main/java/com/maitaidan/flushIPhone/controller/flushIPhoneController.java
+++ src/main/java/com/maitaidan/flushIPhone/controller/flushIPhoneController.java
@@ -1,5 +1,7 @@
 package com.maitaidan.flushIPhone.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,10 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class flushIPhoneController {
 
+    private Logger logger = LoggerFactory.getLogger(flushIPhoneController.class);
     @RequestMapping(value = "addTask")
     @ResponseBody
-    public String addTask(String email, String keyword) {
-        System.out.println("hellpo");
+    public String addTask(String email, String keyword,String type) {
+        logger.info("参数:{}{}{}",email,keyword,type);
         return null;
     }
 }
---
+++ src/main/java/com/maitaidan/flushIPhone/util/HttpRequestUtil.java
@@ -0,0 +1,7 @@
+package com.maitaidan.flushIPhone.util;
+
+/**
+ * Created by xinyu.jiang on 2015/10/9.
+ */
+public class HttpRequestUtil {
+}
