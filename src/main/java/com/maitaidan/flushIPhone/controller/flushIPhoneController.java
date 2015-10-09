19ed2c0d8dd9c1833d39cbe47c55bd550ec352d4
xinyu.jiang <xinyu.jiang@qunar.com>
2015-10-09 20:29:01 +0800

    http工具

--- src/main/java/com/maitaidan/flushIPhone/controller/flushIPhoneController.java
+++ src/main/java/com/maitaidan/flushIPhone/controller/flushIPhoneController.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by xinyu.jiang on 2015/10/9.
- */
 
 @Controller
 public class flushIPhoneController {
